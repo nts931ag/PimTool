e6faa543fb7c17a0cf46977e891f45079624f390
nts931ag <ntson931@gmail.com>
2022-07-11 16:15:56 +0700

    add scope "prototype" for been in AppConfig Class

--- server/src/main/java/com/elca/internship/server/configuration/AppConfig.java
+++ server/src/main/java/com/elca/internship/server/configuration/AppConfig.java
@@ -2,6 +2,7 @@ package com.elca.internship.server.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
@@ -10,11 +11,13 @@ import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 public class AppConfig {
 
     @Bean
+    @Scope("prototype")
     SimpleJdbcInsert getSimpleJdbcInsert(JdbcTemplate jdbcTemplate){
         return new SimpleJdbcInsert(jdbcTemplate);
     }
 
     @Bean
+    @Scope("prototype")
     NamedParameterJdbcTemplate getNamedParameterJdbcTemplate(JdbcTemplate jdbcTemplate){
         return new NamedParameterJdbcTemplate(jdbcTemplate);
     }
